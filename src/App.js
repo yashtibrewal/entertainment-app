eaf68427e2e5c7e9f182346e4ca774b35097bdb4
Nitish814112 <n814112@gmail.com>
2024-11-22 16:14:42 +0530

    Search UI

--- src/App.js
+++ src/App.js
@@ -8,22 +8,35 @@ import TVSeries from "./pages/TVSeries";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Navbar from "./components/navbar";
+import Search from "./components/search";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex flex-col md:flex-row h-screen">
-        <Navbar />
+     
+      <div className="main-container flex flex-col md:flex-row min-h-screen bg-black text-white">
+        {/* Navbar */}
+        <div className="w-full md:w-16 h-auto md:h-screen">
+          <Navbar />
+        </div>
 
-        <div className="flex-1 p-5 md:h-screen sm:h-auto">
-          <Routes>
-            <Route index path="/" element={<HomePage />} />
-            <Route path="/bookmark" element={<Bookmarks />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/tv-series" element={<TVSeries />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
+        <div className=" main-container flexing flex flex-col flex-1">
+          {/* Search Component */}
+          <div className="padding flex-shrink-0 p-4">
+            <Search />
+          </div>
+
+          {/* Routes */}
+          <div className="flexing flex-1 px-16 overflow-auto md:mt-8">
+            <Routes>
+              <Route index path="/" element={<HomePage />} />
+              <Route path="/bookmark" element={<Bookmarks />} />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/tv-series" element={<TVSeries />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </BrowserRouter>
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,7 +1,26 @@
+import { FaSearch } from "react-icons/fa";
+import "../App.css"
+
+
 export default function Search () {
   return (
-    <div>
-      Search bar
+    <div className="flex gap-2 justify-center  mt-4 ml-4 sm:justify-start sm:ml-8">
+     <div>
+     <FaSearch
+     className="w-7 h-5 mt-3 text-white "
+      />
+
+     </div>
+     <div>
+     <input 
+  type="search" 
+  name="search" 
+  id="search" 
+  autoFocus 
+  placeholder="Search for movies or TV series" 
+  className="h-10 w-full sm:w-96 min-w-[200px] text-xl bg-black text-white outline-none px-3"
+/>
+     </div>
     </div>
   )
 }
