add52409d0ce64dd6c266e04829c91db3688bcfe
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-11-23 18:28:15 +0530

    Connected Login to backend

---
+++ src/pages/Login/api.js
@@ -0,0 +1,38 @@
+
+
+const loginUser = async (email, password) => {
+  // Set headers with dynamic token
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  // Set the body with dynamic data (name, email, password)
+  const raw = JSON.stringify({
+    email: email,
+    password: password
+  });
+
+  // Configure request options
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
+
+  try {
+    // Send request and handle the response
+    const response = await fetch("http://localhost:4000/user/login", requestOptions);
+    const result = await response.json()
+    if (response.status === 200) {
+      result.isSuccess = true;
+    } else {
+      result.isSuccess = false;
+    }
+    return result;
+  } catch (error) {
+    console.error('Error: ', error);
+    return { isSuccess: false, message: error.message };
+  }
+};
+
+export default loginUser;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import MovieCreationIcon from '@mui/icons-material/MovieCreation';
+import { useNavigate } from 'react-router-dom';
+import loginUser from './api';
 
 const Login = () => {
   // Initialize AOS
@@ -12,9 +14,23 @@ const Login = () => {
     });
   }, []);
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const login = async () => {
+    const result = await loginUser(email,password);
+
+    if(result.isSuccess) {
+      navigate("/");
+    }else{
+      // TODO: Toast this message
+      console.log(result.message);
+    }
+  }
+
   return (
     <div
-      
       className="bg-gray-900 w-[100vw] h-[100vh] flex flex-col justify-center items-center text-white"
     >
       {/* Icon Section */}
@@ -29,11 +45,13 @@ const Login = () => {
         <h1 className="text-2xl font-semibold mb-6">Login</h1>
 
         {/* Form */}
-        <form className="w-full flex flex-col gap-2">
+        <div className="w-full flex flex-col gap-2">
           <div className="relative">
             <input
               type="email"
               name="email"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
               placeholder="Email address"
               className="bg-transparent border-b-2 border-gray-600 w-full text-sm py-2 px-1 focus:outline-none focus:border-red-500"
             />
@@ -42,17 +60,20 @@ const Login = () => {
             <input
               type="password"
               name="password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
               placeholder="Password"
               className="bg-transparent border-b-2 border-gray-600  w-full text-sm py-2 px-1 focus:outline-none focus:border-red-500"
             />
           </div>
           <button
-            type="submit"
+            type="button"
+            onClick={login}
             className="bg-red-500 hover:bg-red-600 text-white py-2 gap-4 rounded-lg text-sm font-medium transition-all"
           >
             Login to your account
           </button>
-        </form>
+        </div>
 
         {/* Additional Links */}
         <p className="text-sm text-gray-400 mt-4 text-center">
