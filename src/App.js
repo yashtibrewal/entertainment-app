c5f1bf87e7e9bd72affcb5420146b6820c21be97
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-11-24 16:50:50 +0530

    Refactored to Layout

--- src/App.js
+++ src/App.js
@@ -12,33 +12,26 @@ import Search from "./components/search";
 import { AuthProvider } from "./store/auth";
 import ProtectedRoute from "./store/ProtectedRoute";
 import Logout from "./pages/Logout";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <div className="main-container flex flex-col md:flex-row min-h-screen bg-black text-white">
-          {/* Navbar */}
-          <Navbar className="w-full md:w-16 h-auto md:h-screen" />
-          <div className=" main-container flexing flex flex-col flex-1">
-            {/* Search Component */}
-            <Search className="padding flex-shrink-0 p-4" />
-            {/* Routes */}
-            <div className="flexing flex-1 px-16 overflow-auto md:mt-8">
-              <Routes>
-                <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/login" element={<Login />} />
-                <Route element={<ProtectedRoute />}>
-                  <Route index path="/" element={<HomePage />} />
-                  <Route path="/bookmark" element={<Bookmarks />} />
-                  <Route path="/movies" element={<Movies />} />
-                  <Route path="/tv-series" element={<TVSeries />} />
-                  <Route path="/logout" element={<Logout />} />
-                </Route>
-              </Routes>
-            </div>
-          </div>
-        </div>
+        <Routes>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route index path="/login" element={<Login />} />
+          // TODO: Fix protected route
+          {/* <Route element={<ProtectedRoute />}> */}
+            <Route element={<Layout />}>\
+              <Route path="/" element={<HomePage />} />
+              <Route path="/bookmark" element={<Bookmarks />} />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/tv-series" element={<TVSeries />} />
+            </Route>
+          {/* </Route> */}
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
 
