3af3c2ff210bb9d09b783591d9b07ce25a677ee7
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-11-24 14:21:21 +0530

    Segregated protected route to general routes.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Navbar from "./components/navbar";
 import Search from "./components/search";
-import { AuthProvider, useAuth } from "./store/auth";
+import { AuthProvider } from "./store/auth";
 import ProtectedRoute from "./store/ProtectedRoute";
 import Logout from "./pages/Logout";
 
@@ -19,26 +19,22 @@ function App() {
       <AuthProvider>
         <div className="main-container flex flex-col md:flex-row min-h-screen bg-black text-white">
           {/* Navbar */}
-          <div className="w-full md:w-16 h-auto md:h-screen">
-            <Navbar />
-          </div>
+          <Navbar className="w-full md:w-16 h-auto md:h-screen" />
           <div className=" main-container flexing flex flex-col flex-1">
             {/* Search Component */}
-            <div className="padding flex-shrink-0 p-4">
-              <Search />
-            </div>
+            <Search className="padding flex-shrink-0 p-4" />
             {/* Routes */}
             <div className="flexing flex-1 px-16 overflow-auto md:mt-8">
               <Routes>
                 <Route path="/sign-up" element={<SignUp />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route index path="/" element={<HomePage />} />
-                <Route path="/bookmark" element={<Bookmarks />} />
-                <Route path="/movies" element={<Movies />} />
-                <Route path="/tv-series" element={<TVSeries />} />    
-                {/* <Route element={<ProtectedRoute />}>
-                </Route> */}
+                <Route element={<ProtectedRoute />}>
+                  <Route index path="/" element={<HomePage />} />
+                  <Route path="/bookmark" element={<Bookmarks />} />
+                  <Route path="/movies" element={<Movies />} />
+                  <Route path="/tv-series" element={<TVSeries />} />
+                  <Route path="/logout" element={<Logout />} />
+                </Route>
               </Routes>
             </div>
           </div>
