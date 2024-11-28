f9e170dbab57f30d59ae5d5b667a83981b72b4e2
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-11-28 08:43:12 +0530

    Refactored

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,17 +1,18 @@
 // src/components/ProtectedRouteWrapper.js
 import { Navigate } from 'react-router-dom';
-import { useAuth } from './auth';  // Assuming you have the useAuth hook
+import { useAuth } from '../store/auth';  // Assuming you have the useAuth hook
 
-const ProtectedRouteWrapper = ({ children }) => {
-  const { user } = useAuth();  // Access the authentication state
-
-  if (!user) {
+const ProtectedRoute = ({ children }) => {
+  const { state } = useAuth();  // Access the authentication state
+  if (!state.user) {
     // If there's no user (i.e., the user is not logged in), redirect to the login page
     return <Navigate to="/login" replace />;
   }
-
   // If the user is authenticated, render the children (protected route content)
-  return <>{children}</>;
+  console.log(children)
+  return (<div>
+    {children}
+  </div>);
 };
 
-export default ProtectedRouteWrapper;
+export default ProtectedRoute;
