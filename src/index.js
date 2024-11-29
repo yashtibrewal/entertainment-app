59c9f4ff0cd136bea424806b2f762ab51e8da5e7
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-11-29 15:51:43 +0530

    adding strict mode

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ import Store from './Store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={Store}>
-    <BrowserRouter future={{ v7_startTransition: true }}>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={Store}>
+      <BrowserRouter future={{ v7_startTransition: true }}>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
