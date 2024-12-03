0495c97f92e87103daf95955f0c25a0e30808291
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-12-03 21:54:36 +0530

    Search implemented 1. Home Page 2. Movies Page 3. TV-Serieis Page
    Remaining
    1. Home Page (bugged)
    2. Bookmark Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,21 +7,44 @@ import style from './../components/common-media/content.module.css';
 
 import Trending from './TrendingMovies/trending';
 import List from '../components/Content/List'
+import { clearSearchResults } from '../store/Redux/SearchSlice';
 
 
 export default function HomePage() {
 
   const dispatch = useDispatch();
-  const { popularMovies, trendingMovies, movieBookmarks, loading: moviesLoading,
+  const { searchedMovies, popularMovies, trendingMovies, movieBookmarks, loading: moviesLoading,
           error: moviesError } = useSelector((state) => state.movies);
+  const { loading } = useSelector((state) => state.search);
   const [popMovies, setPopMovies] = useState([]);
   const [trendingMoviesLocal, setTrendingMoviesLocal] = useState([]);
 
+  const [trendingMoviesCache, setTrendingMoviesCache] = useState([]);
+  const [popularMoviesCache, setPopularMoviesCache] = useState([]);
 
   useEffect(() => {
     dispatch(fetchAllMovies());
     dispatch(fetchAllMovieBookmarks());
-  }, [dispatch]);
+  }, []);
+
+  useEffect(()=> {
+    // Add the present movies to a cache,
+    // Show the searched movies.
+
+    console.log(searchedMovies.length);
+    if(searchedMovies.length > 0) {
+      setPopularMoviesCache([...popMovies]);
+      setTrendingMoviesCache([...trendingMoviesLocal]);
+      setPopMovies([]);
+      setTrendingMoviesLocal([]);
+      
+      console.log('searchedMovies', searchedMovies);
+    }else {
+      dispatch(clearSearchResults);
+      setPopMovies(popularMoviesCache);
+      setTrendingMoviesLocal(trendingMoviesCache);
+    }
+  }, [searchedMovies.length])
 
   const populateBookmark = useCallback((movie) => {
     let bookmark = false;
@@ -50,28 +73,36 @@ export default function HomePage() {
   if (moviesLoading) return <p>Loading movies...</p>;
   if (moviesError) return <p>Error: {moviesError}</p>;
 
-  return (
+  /**
+   * To only show searched results.
+   * @returns html template
+   */
+  const renderSearchedMovies = () => (
+    <div className="md:ml-4 p-4 max-w-[calc(100vw-120px)] home-width">
+    <div className={style.content}>
+      {searchedMovies.map((card, index) => (
+        <div key={index}>
+          <List cards={[card]} />
+        </div>
+      ))}
+    </div>
+  </div>)
 
+  console.log(searchedMovies.length);
+  if(searchedMovies.length) return renderSearchedMovies();
+
+  return (
     <div className="overflow-x-hidden">
       {/* Trending Section */}
       <div className="md:ml-4 p-4 max-w-[calc(100vw-120px)]">
         <Trending trendingMovies={trendingMoviesLocal} />
       </div>
 
-      {/* Recommended Section  */}
       <div className="md:ml-4 p-4 max-w-[calc(100vw-120px)] home-width">
         <h1 className="mb-4 font-semibold text-2xl text-white">Recommended for you</h1>
-
-        {/* Responsive grid layout for Recommended */}
         <div className={style.content}>
-          {popMovies.map((card, index) => (
-            <div key={index}>
-              <List card={[card]} />
-            </div>
-          ))}
+          <List cards={popMovies} />
         </div>
       </div>
     </div>
-
-  );
-}
+)}
