af50434f5bc2765548f2f334cb6a7f3efd3ce1c4
Nitish814112 <n814112@gmail.com>
2024-12-09 14:54:06 +0530

    fixed: closes #49

--- src/components/Content/List.jsx
+++ src/components/Content/List.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import '../../App.css'
+import "../../App.css";
 import Card from "./Card";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const List = ({ cards }) => {
 
@@ -14,7 +14,6 @@ const List = ({ cards }) => {
     <>
       {
         filteredCards.map((card) => (
-          // TODO: add this in best practice
           <Card key={uuidv4()} {...card}
           />
         ))
