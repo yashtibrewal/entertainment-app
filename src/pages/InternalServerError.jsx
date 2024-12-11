89441426eaa9e05edfc95af40e9dc5d515e47f73
Yash <yashkush.tibrewal@gmail.com>
2024-12-11 18:49:26 +0530

    Added more cases, and handled unauth case.

---
+++ src/pages/InternalServerError.jsx
@@ -0,0 +1,9 @@
+/**
+ * All 500 responses should be diverted to this page.
+ */
+export function InternalServerError() {
+    return <div>
+                Opps. We missed this case.
+           </div>
+    
+}
