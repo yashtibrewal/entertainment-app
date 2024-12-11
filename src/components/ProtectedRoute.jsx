faf0e5c9599d62108772c5298a10fc3e3fa8d1d2
Nitish814112 <n814112@gmail.com>
2024-12-11 17:52:50 +0530

    fixes #2

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,14 +1,23 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useAuth } from '../store/auth';
 
 const ProtectedRoute = () => {
-  const navigate = useNavigate()
   const { state } = useAuth();
 
-  if(state.loading)
-    return <div>Checking Auth</div>;
+  if (state.loading) {
+    return <div>Checking Auth...</div>; // Display loading indicator while authentication status is being checked
+  }
+/// if user is logged out show unauthorised
+  if (!state.isLoggedIn) {
+    return (
+      <div className="unauthorized">
+        <h1>Unauthorized</h1>
+        <p>You do not have access to this page. Please log in.</p>
+      </div>
+    );
+  }
 
-  return state.isLoggedIn ? <Outlet></Outlet> : navigate("/login");
+  return <Outlet />; 
 };
 
 export default ProtectedRoute;
