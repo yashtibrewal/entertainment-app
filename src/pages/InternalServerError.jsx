0fa341393f06acd977abeccdcce911420da698d1
Nitish814112 <n814112@gmail.com>
2024-12-12 16:36:23 +0530

    implemented #77-500-internal-server-page

--- src/pages/InternalServerError.jsx
+++ src/pages/InternalServerError.jsx
@@ -2,8 +2,20 @@
  * All 500 responses should be diverted to this page.
  */
 export function InternalServerError() {
-    return <div>
-                Opps. We missed this case.
-           </div>
-    
+    return (
+        <div className="bg-black">
+            <div className="flex flex-col justify-center items-center h-screen">
+                <img 
+                    src="/8488196.webp" 
+                    alt="An animation illustrating a server error" 
+                    className="w-[350px] mt-2"
+                />
+
+                {/* Text content */}
+                <div className="text-white text-2xl p-8 rounded-lg text-center">
+                    <p className="text-xl mt-4 font-bold">Give it another shot shortly!</p>
+                </div>
+            </div>
+        </div>
+    );
 }
