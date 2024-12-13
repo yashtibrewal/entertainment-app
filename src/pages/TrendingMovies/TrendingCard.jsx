9f15a1145422d4b317b99d965ea780544c620bf4
Yash <yashkush.tibrewal@gmail.com>
2024-12-13 15:21:51 +0530

    Using the first approach for the moment. https://github.com/yashtibrewal/entertainment-app-frontend/issues/45#issuecomment-2541019803

--- src/pages/TrendingMovies/TrendingCard.jsx
+++ src/pages/TrendingMovies/TrendingCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { RiFilmFill } from "react-icons/ri";
 import { BASE_IMAGE_URL } from "../../constants";
-import { Bookmark } from "../../components/common-media/Bookmark";
-import styles from "../../components/common-media/content.module.css";
-import {v4 as uuidv4} from 'uuid';
+// import { Bookmark } from "../../components/common-media/Bookmark";
+// import styles from "../../components/common-media/content.module.css";
+// import {v4 as uuidv4} from 'uuid';
 
 // This card is the wider card used compared to other cards. These cards are placed first.
 const TrendingCard = ({ id, poster_path, bookmark, name, title,first_air_date  ,release_date, adult,media_type}) => {
@@ -14,9 +14,9 @@ const TrendingCard = ({ id, poster_path, bookmark, name, title,first_air_date  ,
 
       <div className="relative">
         <img src={`${BASE_IMAGE_URL}${poster_path}`} alt={title} className="w-full h-48 object-cover" />
-        <Bookmark
+        {/* <Bookmark
           className={styles.bookmark}
-          id={id} bookmark={bookmark} media_type={media_type} key={uuidv4()}></Bookmark>
+          id={id} bookmark={bookmark} media_type={media_type} key={uuidv4()}></Bookmark> */}
           <div className="absolute inset-0 bg-black bg-opacity-25 hover:bg-opacity-0"></div>
         <div className="bottom-2 left-2 absolute bg-transparent px-2 py-1 rounded text-black text-sm">
           <ul className="flex gap-4 px-2 pl-0">
