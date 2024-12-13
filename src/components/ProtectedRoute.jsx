dc18ab0d868507dd181f40f1f1d385abfa4b5496
Nitish814112 <n814112@gmail.com>
2024-12-13 17:17:23 +0530

    handled authorised

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,17 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { useAuth } from '../store/auth';
 import LoaderSpinner from './LoaderSpinner';
+import { UnAuthorized } from "../pages/UnAuthorizedPage";
+
 
 const ProtectedRoute = () => {
+  const location = useLocation();
   const { state } = useAuth();
 
   if (state.loading) {
     return <LoaderSpinner/>
   }
-/// if user is logged out show unauthorised
-  if (!state.isLoggedIn) {
-    return <LoaderSpinner/>;
+
+if (!state.isLoggedIn) {
+  
+  if (location.pathname === "/") {
+    return <LoaderSpinner />;
   }
+  return <UnAuthorized/>;
+}
 
   return <Outlet />; 
 };
