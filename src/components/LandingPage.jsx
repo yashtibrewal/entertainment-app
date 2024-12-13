c19821760d56d9fcd1e224a8dd8f57daac28cd3c
Nitish814112 <n814112@gmail.com>
2024-12-13 19:03:23 +0530

    forget to add files

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
       <div className='absolute inset-0 bg-black bg-opacity-80'></div>
         <header className="header relative z-20 w-[60%] ml-[40%] flex justify-between p-5">
         <span className="text-6xl font-semibold text-center uppercase leading-[100%] relative mix-blend-difference  text-userHover">FlixHub</span>
-            <Link to={'/login'} id="sign-up-btn" className='bg-red-800 border-none outline-none p-2 font-bold text-white rounded-[30px] cursor-pointer hover:opacity-80 font-serif h-10 group-hover:bg-[#07a6f1]  px-7 py-2 transition-all duration-1000 hover:shadow-[0_0_5px_#07a6f1,0_0_25px_#07a6f1,0_0_50px_#07a6f1,0_0_100px_#07a6f1] hover:border-0 '>SIGN IN</Link>
+            <Link to={'/login'} id="sign-up-btn" className='btn-sign bg-red-800 border-none outline-none p-2 font-bold text-white rounded-[30px] cursor-pointer hover:opacity-80 font-serif h-10 group-hover:bg-[#07a6f1]  px-7 py-2 transition-all duration-1000 hover:shadow-[0_0_5px_#07a6f1,0_0_25px_#07a6f1,0_0_50px_#07a6f1,0_0_100px_#07a6f1] hover:border-0 '>SIGN IN</Link>
         </header>
        
         <div className="main-contnet relative z-20 flex flex-col justify-center items-center h-[70vh]">
--- src/components/LoaderSpinner.jsx
+++ src/components/LoaderSpinner.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import './LoaderSpinner.css'
 import { useNavigate } from "react-router";
-import Content from "./content";
+import { useAuth } from "../store/auth";
 import LandingPage from "./LandingPage";
-
 
 const LoaderSpinner = () => {
 
+  const { state } = useAuth();
   const [countdown, setCountdown] = useState(3);
   const [showContent, setShowContent] = useState(false);
   const navigate = useNavigate();
@@ -27,11 +27,7 @@ const LoaderSpinner = () => {
   }, [navigate]);
 
   if (showContent) {
-    if(state.isLoggedIn){
-      navigate("/home")
-    }else{
-      navigate("/login");
-    }
+    return <LandingPage/>
   }
  
   return (
